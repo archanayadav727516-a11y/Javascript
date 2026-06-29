4b236adc11169bda5ca223163e4cfbbc5a55a8e7
Archana Yadav <archanayadav27516@gmail.com>
2026-06-29 19:47:15 +0530

    index.js  updated

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-x = 30; 
-console.log(`Value of X:${x}`);
-var x = 10;
-console.log(`Value of X:${X}`);
+// x = 30; 
+// console.log(`Value of X:${x}`);
+// var x = 10;
+// console.log(`Value of X:${X}`);
+
+
+// let a = 0;
+
+// if (a) {
+//     console.log("truthy");
+
+// }else{
+//     console.log("falsy");
+// }
+
+// let score = 78;
+// let grade = score >=90 ? "A" : score >=75 ? "B" : score >=
+// 60 ? "C" : "Fail";
+// console.log(grade);
+
+
+// let points = 60;
+// let status = points > 100 ? "Gold" : points > 50 ? "Silver"
+// : " Bronze";
+// console.log(status);
+
+
+let x = 3 ;
+let y= x++;x
+console.dirlog(x,y);
